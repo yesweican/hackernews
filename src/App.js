7ca41b0bd4031a6848c9caf57b4634b10e41d44c
Wei Cheng <40693861+yesweican@users.noreply.github.com>
2020-07-26 00:18:04 -0600

    Update 001

--- src/App.js
+++ src/App.js
@@ -3,24 +3,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const list=[
+    {
+      title:'React',
+      url: 'https://reactjs.org',
+      author:'jordan walke',
+      num_comments:3,
+      points:4,
+      objectID:0
+    },
+    {
+      title:'Redux',
+      url: 'https://redux.js.org',
+      author:'Dan Abramov, Andrew Clark',
+      num_comments:2,
+      points:5,
+      objectID:1     
+    }
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React - Hello World!
-        </a>
-      </header>
+         <img src={logo} alt="Logo" width="90" height="90" />
+
+      {
+        list.map(function(item){
+          return(
+          <div>            
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>            
+          );
+        })}
+    </header>    
     </div>
+
   );
+
 }
 
 export default App;
